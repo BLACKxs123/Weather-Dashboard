214bd52f726428b946024221a0f46d49802b5d61
Wesley Alinyo <wesleyalinyo@gmail.com>
2024-08-16 03:47:57 +0300

    Icon Mapping and Search Function

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -16,3 +16,67 @@ const Weather = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [favorites, setFavorites] = useState([]);
 }
+
+const allIcons = {
+  "01d": clear_icon,
+  "01n": clear_icon,
+  "02d": cloud_icon,
+  "02n": cloud_icon,
+  "03d": cloud_icon,
+  "03n": cloud_icon,
+  "04d": drizzle_icon,
+  "04n": drizzle_icon,
+  "09d": rain_icon,
+  "09n": rain_icon,
+  "10d": rain_icon,
+  "10n": rain_icon,
+  "13d": snow_icon,
+  "13n": snow_icon,
+};
+
+const search = async (city) => {
+  if (city === "") {
+    alert("Enter City Name");
+    return;
+  }
+
+  try {
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+
+    const weatherResponse = await fetch(weatherUrl);
+    const weatherData = await weatherResponse.json();
+
+    if (!weatherResponse.ok) {
+      alert(weatherData.message);
+      return;
+    }
+
+    const forecastResponse = await fetch(forecastUrl);
+    const forecastData = await forecastResponse.json();
+
+    if (!forecastResponse.ok) {
+      alert(forecastData.message);
+      return;
+    }
+
+    console.log(weatherData);
+    const icon = allIcons[weatherData.weather[0].icon] || clear_icon;
+    setWeatherData({
+      humidity: weatherData.main.humidity,
+      windSpeed: weatherData.wind.speed,
+      temperature: Math.floor(weatherData.main.temp),
+      location: weatherData.name,
+      icon: icon,
+    });
+
+    const dailyForecast = forecastData.list.filter((reading) =>
+      reading.dt_txt.includes("12:00:00")
+    );
+    setForecastData(dailyForecast);
+
+  } catch (error) {
+    setWeatherData(false);
+    console.error("Error in fetching weather data:", error);
+  }
+};
